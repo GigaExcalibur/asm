3ec54b5d94b46a7627e1c6ea0adebc14fa94a081
Dragz <jlevitt8962@gmail.com>
2025-07-14 12:24:54 -0700

    fix stealing sounds effects

--- UnitNameItemDrop/UnitNameItemDrop.c
+++ UnitNameItemDrop/UnitNameItemDrop.c
@@ -33,6 +33,22 @@ struct PopupInstruction const NewItemPilferedPopup[] = {
 };
 
 struct PopupInstruction const NewItemStolePopup[] = {
+    POPUP_SOUND(0x5A),
+	POPUP_COLOR(TEXT_COLOR_SYSTEM_BLUE),
+	POPUP_UNIT_NAME,
+	POPUP_COLOR(TEXT_COLOR_SYSTEM_WHITE),
+    POPUP_MSG(0x00A),                   /* Stole */
+	POPUP_COLOR(TEXT_COLOR_SYSTEM_BLUE),
+    POPUP_ITEM_STR,
+    POPUP_SPACE(1),
+    POPUP_ITEM_ICON,
+    POPUP_SPACE(1),
+    POPUP_COLOR(TEXT_COLOR_SYSTEM_WHITE),
+    POPUP_MSG(0x022),                   /* .[.] */
+    POPUP_END
+};
+
+struct PopupInstruction const NewItemWasStolenPopup[] = {
     POPUP_SOUND(0x5C),
 	POPUP_COLOR(TEXT_COLOR_SYSTEM_BLUE),
 	POPUP_UNIT_NAME,
@@ -104,6 +120,11 @@ void NewPopup_ItemStealing(u16 item, ProcPtr parent)
 {
     SetPopupItem(item);
 	SetPopupUnit(gActiveUnit);
-
-    NewPopup_Simple(NewItemStolePopup, 0x60, 0x0, parent);
+	
+	if(FACTION_BLUE == UNIT_FACTION(gActiveUnit)) {
+		NewPopup_Simple(NewItemStolePopup, 0x60, 0x0, parent);
+	}
+    else {
+		NewPopup_Simple(NewItemWasStolenPopup, 0x60, 0x0, parent);
+	}
 }
